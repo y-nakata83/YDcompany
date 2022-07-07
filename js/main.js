4b9142f4e3391b583b18f55f2101707eb9c83192
y-nakata83 <a.and.mi.0815@gmail.com>
2022-07-07 22:18:32 +0900

    git up

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.header_nav , .btn_img').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  })
+});
+
+function PageTopCheck(){
+    var winScrollTop = $(this).scrollTop();
+    var secondTop =  $("#service").offset().top - 150; //#area-2の上から150pxの位置まで来たら
+    if(winScrollTop >= secondTop){
+		$('.js-scroll').removeClass('scroll-view');//.js-scrollからscroll-viewというクラス名を除去
+		$('.js-pagetop').addClass('scroll-view');//.js-pagetopにscroll-viewというクラス名を付与
+	} else {//元に戻ったら
+		$('.js-scroll').addClass('scroll-view');//.js-scrollからscroll-viewというクラス名を付与
+		$('.js-pagetop').removeClass('scroll-view');//.js-pagetopからscroll-viewというクラス名を除去
+	}
+
+}
