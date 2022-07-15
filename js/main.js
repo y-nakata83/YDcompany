9a93f4f9b8bc5fac18df942f75d8e30e8100e235
y-nakata83 <a.and.mi.0815@gmail.com>
2022-07-15 14:25:30 +0900

    header固定＆loadmove追加

--- js/main.js
+++ js/main.js
@@ -8,5 +8,34 @@ function PageTopCheck(){
 		$('.js-scroll').addClass('scroll-view');//.js-scrollからscroll-viewというクラス名を付与
 		$('.js-pagetop').removeClass('scroll-view');//.js-pagetopからscroll-viewというクラス名を除去
 	}
-
 }
+
+var navPos = jQuery( '.header_under' ).offset().top; // グローバルメニューの位置
+var navHeight = jQuery( '.header_under' ).outerHeight(); // グローバルメニューの高さ
+jQuery( window ).on( 'scroll', function() {
+	if ( jQuery( this ).scrollTop() > navPos ) {
+		jQuery( 'body' ).css( 'padding-top', navHeight );
+		jQuery( '.header_under' ).addClass( 'm_fixed' );
+	} else {
+		jQuery( 'body' ).css( 'padding-top', 0 );
+		jQuery( '.header_under' ).removeClass( 'm_fixed' );
+	}
+});
+
+$(window).on('load', function(){
+  $('body').removeClass('fadeout');
+});
+$(function() {
+  // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+  $('a:not([href^="#"]):not([target])').on('click', function(e){
+    e.preventDefault(); // ナビゲートをキャンセル
+    url = $(this).attr('href'); // 遷移先のURLを取得
+    if (url !== '') {
+      $('body').addClass('fadeout');  // bodyに class="fadeout"を挿入
+      setTimeout(function(){
+        window.location = url;  // 0.8秒後に取得したURLに遷移
+      }, 800);
+    }
+    return false;
+  });
+});
